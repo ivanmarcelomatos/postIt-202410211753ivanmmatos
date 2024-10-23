706b10c498dbbf24795a8eb09b8fb52832a97a50
ivanmarcelomatos <ivanmarcelomatos@gmail.com>
2024-10-23 13:55:52 -0300

    added handling for possible MongoDB connection errors

--- backend/src/config/dbConfig.js
+++ backend/src/config/dbConfig.js
@@ -3,6 +3,20 @@ require('dotenv').config();
 
 const uri = process.env.MONGODB_URI;
 
+if (!uri) {
+    console.error('MongoDB URI is not defined in environment variables!');
+    process.exit(1); // Encerrar a aplicação se a URI não estiver definida
+}
+
 const connection = mongoose.connect(uri);
+
+connection
+    .then(() => {
+        console.log('Successfully connected to MongoDB');
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
+
 
 module.exports = connection;
