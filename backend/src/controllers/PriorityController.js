911d618c951dde01a07baf19051b039158d5be9d
ivanmarcelomatos <ivanmarcelomatos@gmail.com>
2024-10-25 12:00:22 -0300

    structured a controller containing a read method that can be used in routes to fetch annotations based on the priority filter

---
+++ backend/src/controllers/PriorityController.js
@@ -0,0 +1,12 @@
+const Annotations = require('../models/AnnotationData');
+
+module.exports = {
+
+    async read(request, response) {
+        const priority = request.query;
+        const priorityNotes = await Annotations.find(priority);
+
+        return response.json(priorityNotes);
+    }
+
+};
