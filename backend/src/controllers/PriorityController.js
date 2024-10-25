99e5cbd65395d32f36ba78dc7dc7d0b661de0439
ivanmarcelomatos <ivanmarcelomatos@gmail.com>
2024-10-25 15:11:52 -0300

    created an update method that toggles the annotation's priority field based on its current value

--- backend/src/controllers/PriorityController.js
+++ backend/src/controllers/PriorityController.js
@@ -7,6 +7,21 @@ module.exports = {
         const priorityNotes = await Annotations.find(priority);
 
         return response.json(priorityNotes);
+    },
+
+    async update(request, response) {
+        const { id } = request.params;
+        const annotation = await Annotations.findOne({ _id: id });
+        
+        if (annotation.priority) {
+            annotation.priority = false;
+        } else {
+            annotation.priority = true;
+        };
+
+        await annotation.save();
+
+        return response.json(annotation);
     }
 
 };
