e0040d42aa6a73cf4aa8d8e24b2c51f3051c7058
ivanmarcelomatos <ivanmarcelomatos@gmail.com>
2024-10-27 07:37:09 -0300

    registered a POST route for updating content through the ContentController's update method

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,7 @@ const routes = express.Router();
 
 const AnnotationController = require('./controllers/AnnotationController');
 const PriorityController = require('./controllers/PriorityController');
+const ContentController = require('./controllers/ContentController');
 
 routes.post('/annotations', AnnotationController.create);
 routes.get('/annotations', AnnotationController.read);
@@ -10,5 +11,7 @@ routes.delete('/annotations/:id', AnnotationController.delete);
 
 routes.get('/priorities', PriorityController.read);
 routes.post('/priorities/:id', PriorityController.update);
+
+routes.post('/contents/:id', ContentController.update);
 
 module.exports = routes;
