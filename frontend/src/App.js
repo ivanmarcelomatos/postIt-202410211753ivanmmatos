e29980950188caf9bda979abb47e1162cf379e6a
ivanmarcelomatos <ivanmarcelomatos@gmail.com>
2024-10-29 20:27:24 -0300

    example usage of 'React.Fragment'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,13 @@
+import React from 'react';
+import Header from './Header';
 
 function App() {
   return (
-    <h1>DayNotes</h1>
+    <>
+      <Header title='my note 1'/>
+      <Header title='my note 2'/>
+      <Header title='my note 3'/>
+    </>
   );
 }
 
