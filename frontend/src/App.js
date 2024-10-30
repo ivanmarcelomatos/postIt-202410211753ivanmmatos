5305b8519061bc3c61e105953df4fd903ba9441d
ivanmarcelomatos <ivanmarcelomatos@gmail.com>
2024-10-30 16:51:19 -0300

    created element '<main>'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,7 +21,20 @@ function App() {
           <button type="submit">Save</button>
         </form>
       </aside>
-      <main></main>
+      <main>
+        <ul>
+          <li className="notepad-infos">
+            <div>
+              <strong>Fazer Compras</strong>
+              <div>
+                x
+              </div>
+            </div>
+            <textarea>lorem20 asdf asdf 2 asdqweur asd;fljasdf qw8e</textarea>
+            <span>!</span>
+          </li>
+        </ul>
+      </main>
     </div>
   )
 }
