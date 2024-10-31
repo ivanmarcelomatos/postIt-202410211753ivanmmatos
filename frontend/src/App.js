bb91a21c599e17aa1ebefd107e9c0b5fb73472f6
ivanmarcelomatos <ivanmarcelomatos@gmail.com>
2024-10-31 16:21:13 -0300

    populating the '<ul>' for educational purposes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -35,6 +35,46 @@ function App() {
             <textarea>lorem20 asdf asdf 2 asdqweur asd;fljasdf qw8e</textarea>
             <span>!</span>
           </li>
+          <li className="notepad-infos">
+            <div>
+              <strong>Fazer Compras</strong>
+              <div>
+                x
+              </div>
+            </div>
+            <textarea>lorem20 asdf asdf 2 asdqweur asd;fljasdf qw8e</textarea>
+            <span>!</span>
+          </li>
+          <li className="notepad-infos">
+            <div>
+              <strong>Fazer Compras</strong>
+              <div>
+                x
+              </div>
+            </div>
+            <textarea>lorem20 asdf asdf 2 asdqweur asd;fljasdf qw8e</textarea>
+            <span>!</span>
+          </li>
+          <li className="notepad-infos">
+            <div>
+              <strong>Fazer Compras</strong>
+              <div>
+                x
+              </div>
+            </div>
+            <textarea>lorem20 asdf asdf 2 asdqweur asd;fljasdf qw8e</textarea>
+            <span>!</span>
+          </li>
+          <li className="notepad-infos">
+            <div>
+              <strong>Fazer Compras</strong>
+              <div>
+                x
+              </div>
+            </div>
+            <textarea>lorem20 asdf asdf 2 asdqweur asd;fljasdf qw8e</textarea>
+            <span>!</span>
+          </li>
         </ul>
       </main>
     </div>
