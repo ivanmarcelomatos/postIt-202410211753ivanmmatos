0fe687a92f7862edc64e8613e870912d3ac9ee1a
ivanmarcelomatos <ivanmarcelomatos@gmail.com>
2024-11-02 09:26:44 -0300

    Fixed browser console warning that in React, <textarea> elements should use the 'value' or 'defaultValue' properties to control their content, instead of setting the initial text directly as children, and eliminated repetitive hard code

--- frontend/src/Components/Notes/index.js
+++ frontend/src/Components/Notes/index.js
@@ -10,47 +10,7 @@ function Notes() {
                 x
               </div>
             </div>
-            <textarea>lorem20 asdf asdf 2 asdqweur asd;fljasdf qw8e</textarea>
-            <span>!</span>
-          </li>
-          <li className="notepad-infos">
-            <div>
-              <strong>Fazer Compras</strong>
-              <div>
-                x
-              </div>
-            </div>
-            <textarea>lorem20 asdf asdf 2 asdqweur asd;fljasdf qw8e</textarea>
-            <span>!</span>
-          </li>
-          <li className="notepad-infos">
-            <div>
-              <strong>Fazer Compras</strong>
-              <div>
-                x
-              </div>
-            </div>
-            <textarea>lorem20 asdf asdf 2 asdqweur asd;fljasdf qw8e</textarea>
-            <span>!</span>
-          </li>
-          <li className="notepad-infos">
-            <div>
-              <strong>Fazer Compras</strong>
-              <div>
-                x
-              </div>
-            </div>
-            <textarea>lorem20 asdf asdf 2 asdqweur asd;fljasdf qw8e</textarea>
-            <span>!</span>
-          </li>
-          <li className="notepad-infos">
-            <div>
-              <strong>Fazer Compras</strong>
-              <div>
-                x
-              </div>
-            </div>
-            <textarea>lorem20 asdf asdf 2 asdqweur asd;fljasdf qw8e</textarea>
+            <textarea defaultValue="lorem20 asdf asdf 2 asdqweur asd;fljasdf qw8e"></textarea>
             <span>!</span>
           </li>  
         </>
