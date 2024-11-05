f4959e839068cc8f89aa80e69e323db1ecfb2be9
ivanmarcelomatos <ivanmarcelomatos@gmail.com>
2024-11-05 20:33:01 -0300

    keeps track of any changes made to the note's content; Create 'handleSave' Function that will be triggered when the user leaves (blurs) the textarea to update the note on the server

--- frontend/src/Components/Notes/index.js
+++ frontend/src/Components/Notes/index.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AiTwotoneDelete, AiOutlineExclamationCircle } from "react-icons/ai";
 
 import './styles.css';
 import './styles-priority.css';
 
+import api from '../../services/api';
+
 
 function Notes({data}) {
+    const [ changedNote, setChangedNote ] = useState('');
+
+    async function handleSave(e, notes) {
+
+      if(changedNote && changedNote !== notes) {
+        await api.post(`/contents/${data._id}`, {
+          notes: changedNote,
+        });
+      }
+
+    }
+
     return(
         <>
             <li className={data.priority ? "notepad-infos-priority": "notepad-infos"}>
@@ -15,7 +29,11 @@ function Notes({data}) {
                 <AiTwotoneDelete size="20" />
               </div>
             </div>
-            <textarea defaultValue={ data.notes }></textarea>
+            <textarea 
+              defaultValue={ data.notes }
+              onChange={e => setChangedNote(e.target.value)}
+              onBlur={e => handleSave(e.target, data.notes)}
+            />
             <span>
               <AiOutlineExclamationCircle size="20" />
             </span>
