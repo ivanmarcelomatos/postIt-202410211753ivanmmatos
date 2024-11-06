cf154f2523d84fc6ccddc4d737f7ff73b83e23fb
ivanmarcelomatos <ivanmarcelomatos@gmail.com>
2024-11-06 13:34:28 -0300

    created 'handleEdit Function' that is called when the textarea is clicked, indicating edit mode

--- frontend/src/Components/Notes/index.js
+++ frontend/src/Components/Notes/index.js
@@ -10,6 +10,17 @@ import api from '../../services/api';
 function Notes({data}) {
     const [ changedNote, setChangedNote ] = useState('');
 
+    function handleEdit(e, priority) {
+      e.style.cursor = 'text';
+      e.style.borderRadius = '5px';
+
+      if(priority) {
+        e.style.boxShadow = '0 0 5px white';
+      } else {
+        e.style.boxShadow = '0 0 5px gray';
+      };
+    }
+
     async function handleSave(e, notes) {
 
       if(changedNote && changedNote !== notes) {
@@ -31,6 +42,7 @@ function Notes({data}) {
             </div>
             <textarea 
               defaultValue={ data.notes }
+              onClick={e => handleEdit(e.target, data.priority)}
               onChange={e => setChangedNote(e.target.value)}
               onBlur={e => handleSave(e.target, data.notes)}
             />
