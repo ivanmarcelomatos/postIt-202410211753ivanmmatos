a7fdd56fc20078d51636cfaeafea4d86f8d2f95c
ivanmarcelomatos <ivanmarcelomatos@gmail.com>
2025-06-27 16:11:35 -0300

    implemented improvements to the schema: better data validation, removing white spaces and adding timestamps

--- backend/src/models/AnnotationData.js
+++ backend/src/models/AnnotationData.js
@@ -1,8 +1,20 @@
 const mongoose = require('mongoose');
+
 const AnnotationDataSchema = new mongoose.Schema({
-    title: String,
-    notes: String,
-    priority: Boolean
-});
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  notes: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  priority: {
+    type: Boolean,
+    default: false
+  }
+}, { timestamps: true });
 
 module.exports = mongoose.model('Annotations', AnnotationDataSchema);
